a083b46957315fa7f112a8a60707845a71e3979f
gardenLee <khjvvv7@gmail.com>
2021-08-07 01:09:36 +0900

    jib로 이미지 푸시 테스트

---
+++ build.gradle.kts
@@ -0,0 +1,105 @@
+//plugins {
+//    id 'org.springframework.boot' version '2.5.3'
+//    id 'io.spring.dependency-management' version '1.0.11.RELEASE'
+//    id 'java'
+//    id 'com.google.cloud.tools.jib' version '3.1.2'
+//}
+//
+//group = 'lee.garden'
+//version = '0.0.1-SNAPSHOT'
+//sourceCompatibility = '11'
+//
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    implementation 'org.springframework.boot:spring-boot-starter-web'
+//    testImplementation 'org.springframework.boot:spring-boot-starter-test'
+//}
+//
+//test {
+//    useJUnitPlatform()
+//}
+//
+//jib {
+//    def ver = UUID.randomUUID().toString()
+//    def DOCKER_USERNAME = System.getenv("DOCKER_USERNAME")
+//    def DOCKER_PASSWORD = System.getenv("DOCKER_PASSWORD")
+//
+//    from {
+//        image='openjdk:11-jre-slim'
+//    }
+//
+//    to {
+//        image = "gardenlee/cicd-practice"
+//        auth{
+//            it.setUsername(DOCKER_USERNAME)
+//            it.setPassword(DOCKER_PASSWORD)
+//        }
+//        tags = [ver]
+//    }
+//
+//    container {
+//        mainClass = "lee.garden.cicdtest.CicdTestApplication"
+//        ports = ["8080"]
+//        volumes = ["/tmp"]
+//    }
+//}
+
+plugins {
+    java
+    id("org.springframework.boot") version "2.5.3"
+    id("com.google.cloud.tools.jib") version "3.1.2"
+    id("org.ajoberstar.grgit") version "1.7.2"
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+
+apply(plugin = "io.spring.dependency-management")
+apply(plugin = "java")
+apply(plugin = "org.ajoberstar.grgit")
+
+val grgit: org.ajoberstar.grgit.Grgit by extra
+val buildVersion by extra {grgit.head().abbreviatedId}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    jib {
+
+        from {
+            image = "openjdk:11-jre-slim"
+        }
+
+        to {
+            image = "gardenlee/cicd-practice:${buildVersion}"
+            System.getenv("DOCKER_USERNAME")?.let { envUsername ->
+                System.getenv("DOCKER_PASSWORD")?.let { envPassword ->
+                    auth {
+                        username = envUsername
+                        password = envPassword
+                    }
+                }
+            }
+        }
+
+        container {
+            mainClass = "lee.garden.cicdtest.CicdTestApplication"
+            ports = listOf("8080")
+            volumes = listOf("/tmp")
+    }
+}
+}
--- src/main/java/lee/garden/cicdtest/CicdTestApplication.java
+++ src/main/java/lee/garden/cicdtest/CicdTestApplication.java
@@ -2,12 +2,23 @@ package lee.garden.cicdtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CicdTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdTestApplication.class, args);
+    }
+
+    @RestController
+    class Health {
+
+        @GetMapping("/")
+        public String health() {
+            return "hello world!";
+        }
     }
 
 }
